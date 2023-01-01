1ea21b59f8ef564791f7b6a57fa4e0d603eb4d07
taqiussa <taqiussa@gmail.com>
2023-01-01 12:25:33 +0700

    data pendaftar pagination

--- app/Http/Livewire/Landing.php
+++ app/Http/Livewire/Landing.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Livewire\Component;
 use Livewire\WithPagination;
 use WireUi\Traits\Actions;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 
 class Landing extends Component
 {
@@ -13,18 +14,36 @@ class Landing extends Component
     use WithPagination;
 
     public $search = '';
+    public $page = 1;
+    protected $queryString = [
 
+        'search' => ['except' => ''],
+
+        'page' => ['except' => 1],
+
+    ];
     public function render()
     {
         return view(
             'livewire.landing',
             [
                 'listUser' => User::role('Calon Siswa')
-                    ->where('name', 'like', '%' . $this->search . '%')
+                    ->when(
+                        $this->search,
+                        fn (Builder $query) => $query
+                            ->where('name', 'like', "%{$this->search}%")
+                    )
+                    // ->where('name', 'like', '%' . $this->search . '%')
                     ->with(['alamat', 'alamat.village', 'alamat.district', 'sekolahSd'])
                     ->orderBy('name')
-                    ->paginate(10)
+                    ->paginate(5)
             ]
         )->layout('layouts.guest');
+    }
+
+    public function updatingSearch()
+    {
+
+        $this->resetPage();
     }
 }
