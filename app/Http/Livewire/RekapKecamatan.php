a4e7f184dec8bd63106adfd5aa17960134b5c0da
taqiussa <taqiussa@gmail.com>
2023-01-15 20:35:16 +0700

    rekap perkecamatan

--- app/Http/Livewire/RekapKecamatan.php
+++ app/Http/Livewire/RekapKecamatan.php
@@ -13,10 +13,11 @@ class RekapKecamatan extends Component
     public function render()
     {
 
-        $listKecamatan = Target::with(['district', 'user'])
+        $listKecamatan = Target::with(['district', 'user', 'user.pembayaran'])
             ->withCount([
                 'user as diterima' => fn ($q) => $q->whereDiterima('diterima'),
                 'user as ditolak' => fn ($q) => $q->whereDiterima('tidak diterima'),
+                'user as daftar_ulang' => fn ($q) => $q->whereHas('pembayaran'),
                 'user as total',
             ])
             ->get()
@@ -33,6 +34,10 @@ class RekapKecamatan extends Component
                 'luarDitolak' => SekolahSd::whereNotIn('kecamatan', $idKecamatan)
                     ->with(['user'])
                     ->whereHas('user', fn($q) => $q->whereDiterima('tidak diterima'))
+                    ->count(),
+                'luarDaftarUlang' => SekolahSd::whereNotIn('kecamatan', $idKecamatan)
+                    ->with(['user', 'user.pembayaran'])
+                    ->whereHas('user', fn($q) => $q->whereHas('pembayaran'))
                     ->count(),
                 'luarTotal' => SekolahSd::whereNotIn('kecamatan', $idKecamatan)
                     ->count(),
--- resources/views/livewire/rekap-kecamatan.blade.php
+++ resources/views/livewire/rekap-kecamatan.blade.php
@@ -16,26 +16,38 @@
                     <th class="border border-slate-600 py-1 px-1">Diterima</th>
                     <th class="border border-slate-600 py-1 px-1">Ditolak</th>
                     <th class="border border-slate-600 py-1 px-1">Total Pendaftar</th>
+                    <th class="border border-slate-600 py-1 px-1">Daftar Ulang</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($listKecamatan as $target)
-                    <tr class="font-bold odd:bg-slate-100">
+                    @if ($target->daftar_ulang >= $target->jumlah)
+                        @php
+                            $isTarget = true;
+                        @endphp
+                    @else
+                        @php
+                            $isTarget = false;
+                        @endphp
+                    @endif
+                    <tr  @class(['font-bold odd:bg-slate-100','bg-green-500 odd:bg-green-500 text-white' => $isTarget])>
                         <td class="pl-2 border border-slate-600 py-1 px-1">{{ $loop->iteration }}</td>
                         <td class="pl-2 border border-slate-600 py-1 px-1">{{ $target->district->name }}</td>
                         <td class="pl-2 border border-slate-600 py-1 px-1 text-center">{{ $target->jumlah }}</td>
                         <td class="pl-2 border border-slate-600 py-1 px-1 text-center">{{ $target->diterima }}</td>
                         <td class="pl-2 border border-slate-600 py-1 px-1 text-center">{{ $target->ditolak }}</td>
                         <td class="pl-2 border border-slate-600 py-1 px-1 text-center">{{ $target->total }}</td>
+                        <td class="pl-2 border border-slate-600 py-1 px-1 text-center">{{ $target->daftar_ulang }}</td>
                     </tr>
                 @endforeach
                 <tr class="font-bold">
                     <td class="pl-2 border border-slate-600 py-1 px-1">{{ $listKecamatan->count() + 1 }}</td>
-                        <td class="pl-2 border border-slate-600 py-1 px-1">LUAR KOTA</td>
-                        <td class="pl-2 border border-slate-600 py-1 px-1 text-center">-</td>
-                        <td class="pl-2 border border-slate-600 py-1 px-1 text-center">{{ $luarDiterima }}</td>
-                        <td class="pl-2 border border-slate-600 py-1 px-1 text-center">{{ $luarDitolak }}</td>
-                        <td class="pl-2 border border-slate-600 py-1 px-1 text-center">{{ $luarTotal }}</td>
+                    <td class="pl-2 border border-slate-600 py-1 px-1">LUAR KOTA</td>
+                    <td class="pl-2 border border-slate-600 py-1 px-1 text-center">-</td>
+                    <td class="pl-2 border border-slate-600 py-1 px-1 text-center">{{ $luarDiterima }}</td>
+                    <td class="pl-2 border border-slate-600 py-1 px-1 text-center">{{ $luarDitolak }}</td>
+                    <td class="pl-2 border border-slate-600 py-1 px-1 text-center">{{ $luarTotal }}</td>
+                    <td class="pl-2 border border-slate-600 py-1 px-1 text-center">{{ $luarDaftarUlang }}</td>
                 </tr>
             </tbody>
         </table>
