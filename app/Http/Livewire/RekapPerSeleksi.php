488f241f632833c13cd6703bda3c8dd48940ff90
taqiussa <taqiussa@gmail.com>
2023-01-28 09:27:54 +0700

    rekap per seleksi

---
+++ app/Http/Livewire/RekapPerSeleksi.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class RekapPerSeleksi extends Component
+{
+    public function render()
+    {
+        return view('livewire.rekap-per-seleksi');
+    }
+}
