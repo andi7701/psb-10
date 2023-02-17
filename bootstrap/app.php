8015197a392e6c98a83e3d19bcaf3035b8e87c5e
unknown <taqiussa>
2023-02-17 07:32:48 +0700

    config app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,8 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+$app->register(Laravolt\Indonesia\ServiceProvider::class);
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
