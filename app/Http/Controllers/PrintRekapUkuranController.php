52f5b89b83616a395be57cae4d7cfb6492a3a3ab
taqiussa <taqiussa@gmail.com>
2023-03-27 12:07:06 +0700

    print rekap seragam

--- app/Http/Controllers/PrintRekapUkuranController.php
+++ app/Http/Controllers/PrintRekapUkuranController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PrintRekapUkuranController extends Controller
 {
     public function index()
     {
-        return view('print.rekap-ukuran');
+        return view('print.rekap-ukuran', [
+            'listUser' => User::whereDiterima('diterima')
+                ->whereTerukur(true)
+                ->with(['seragam', 'biodata', 'sekolahSd'])
+                ->get()
+        ]);
     }
 }
